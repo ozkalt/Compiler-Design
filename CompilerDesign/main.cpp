b5d4d77a1b67ed3ba3332b9620c7e940bb95705e
Tugba <tugba@arhenius.com>
2017-12-01 19:33:39 +0300

    Initial Commit

---
+++ CompilerDesign/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CompilerDesign
+//
+//  Created by Tuğba Özkal on 1.12.2017.
+//  Copyright © 2017 Tuğba Özkal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
